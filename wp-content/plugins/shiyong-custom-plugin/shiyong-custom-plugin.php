8505979e3a9b25c227cd31f7bb4f03661896233d
Yusheng Ding <davidthinkleding@Gmail.com>
2018-10-06 15:49:51 -0700

    begin shiyong custom plugin

---
+++ wp-content/plugins/shiyong-custom-plugin/shiyong-custom-plugin.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * @package shiyongCustomPlugin
+ */
+/*
+Plugin Name: Shiyong Custom Plugin
+Plugin URI: http://shiyong.us/plugin
+Description: Add custom functions for Shiyong@us website
+Version: 1.0.0
+Author: David Ding
+Author URI: https://formatmemory.github.io
+License: GPLv2 or Later
+Text Domain: Shiyong Custom Plugin
+*/
+
